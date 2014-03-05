4bea0fead14cca5fcf3fb493c7763b583a4b6fe2
grantmx <support@grantmx.com>
2014-03-05 13:57:34 -0500

    Fix.
    Put the wrong version up for the seed.

--- scripts/registration.controller.js
+++ scripts/registration.controller.js
@@ -4,22 +4,49 @@
 
 	regApp.config(function ($routeProvider) {
 		$routeProvider.when("/", {
-				templateUrl: "index.html",
-				controller: "eventList"
+				templateUrl: "eventlist.html",
+				controller: "EventList"
 			})
 			.when("/registrations/:eventid", {
 				templateUrl: "registrations.html",
-				conroller: "regList"
+				conroller: "RegList"
 			})
 	});
 
 
-	regApp.controller("regList", function ($scope, $log, $route, $routeParams){
-		$scope.people = [
-			{name: "Wilbert Duke", here: false},
-			{name: "Tena Guidi", here: true},
-			{name: "Suana Burstien", here: false}
-		];
+	regApp.controller("RegList", function ($scope, $log, $route, $routeParams, $q){
+		var defer = $q.defer();
+
+		$scope.people = [];
+
+		defer.promise
+			.then(function (eventid){
+				console.log(eventid);
+
+				if(eventid === "678"){
+					$scope.people = [
+						{name: "Wilbert Duke", here: false},
+						{name: "Tena Guidi", here: true},
+						{name: "Suana Burstien", here: false}
+					];
+				}
+
+				if(eventid === "12345"){
+					$scope.people = [
+						{name: "Leona Kamierczak", here: true},
+						{name: "Randolph Overfield", here: true},
+						{name: "Landon Greely", here: false},
+						{name: "Bob Barker", here: false},
+						{name: "Daniel Day", here: false},
+						{name: "Herbert Wolf", here: false},
+						{name: "Jon Bonjovi", here: false},
+						{name: "Grant Marshall", here: false},
+						{name: "Marshall Grant", here: false}
+					];
+				}
+			})
+
+		defer.resolve($routeParams.eventid);
 
 		$scope.postRegistrations = function(){
 			$log.info($scope.people);
@@ -28,10 +55,10 @@
 
 
 
-	regApp.controller("eventList", function ($scope, $route){
+	regApp.controller("EventList", function ($scope, $route){
 		$scope.events = [
-			{name: "XYZ Conference 2014", url: "/registrations/678"},
-			{name: "Kingmakers Conference 2014", url: "/registrations/12345"}
+			{name: "XYZ Conference 2014", url: "registrations/678"},
+			{name: "Conference 2014", url: "registrations/12345"}
 		];
 	});
 
